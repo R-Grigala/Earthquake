0737a95e86638df1e8bae6e6407154d923765d62
geogamura <grigalashvili.r1999@gmail.com>
2022-08-03 15:16:29 +0400

    last update

--- az_proj/settings.py
+++ az_proj/settings.py
@@ -141,6 +141,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_DIR = BASE_DIR /'static'
+STATIC_ROOT = BASE_DIR /'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
